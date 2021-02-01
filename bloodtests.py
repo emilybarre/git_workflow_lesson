20c1777b3bc4e0e307254e33d51897a867166467
Emily Barre <emily.barre@duke.edu>
2021-02-01 15:05:54 -0500

    Added function that prints out the HDL results

--- bloodtests.py
+++ bloodtests.py
@@ -13,7 +13,7 @@ def interface():
 def HDL_driver():
 	HDL_val = getdata()
 	result = analyzedata(HDL_val)
-	output(result)
+	output(HDL_val,result)
 
 def getdata():
 	HDL = input("What is your HDL?")
@@ -21,9 +21,9 @@ def getdata():
 	return HDL
 
 def analyzedata(HDL)
-	if HDL > 60: 
+	if HDL >= 60: 
 		result = "Normal" 
-	elif 40<HDL<60:
+	elif 40 <= HDL < 60:
 		result = "Borderline Low"
 	elif HDL < 40: 
 		result = "Low"
@@ -31,5 +31,9 @@ def analyzedata(HDL)
 		result = "Error"
 		
 	return result
+
+def output(HDL,result):
+	print("The HDL entered was {}".format(HDL))
+	print("This HDL level is {}".format(result))
 	
 interface()
