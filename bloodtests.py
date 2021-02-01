3302dcdd528af792701618a7b29e8f1d6c3a4b99
Emily Barre <emily.barre@duke.edu>
2021-02-01 14:59:18 -0500

    Added HDL driver to interace options

--- bloodtests.py
+++ bloodtests.py
@@ -2,9 +2,12 @@ def interface():
 	print("Blood Test Analysis")
 	while True: 
 		print("\nOptions")
+		print("1 - HDL")
 		print("9 - Quit")
 		choice = input("Enter an option: ")
-		if choice == "9": 
-			return
-		
+	if choice == "9": 
+		return
+	elif choice == "1":
+		HDL_driver()
+
 interface()
