3375feb3917341b0d44321d10b4bd4181378cb4f
Emily Barre <emily.barre@duke.edu>
2021-02-11 17:07:26 -0500

    Made changes to be python style compatible

--- test_bloodtests.py
+++ test_bloodtests.py
@@ -1,27 +1,13 @@
-def test_analyze_HDL_Normal():
-    from bloodtests import analyze_HDL
-    
-    result = analyze_HDL(65)
-    expected = "Normal"
+import pytest
 
-    assert result == expected 
-    
-def test_analyze_HDL_B_Low():
-    from bloodtests import analyze_HDL
-    
-    result = analyze_HDL(50)
-    expected = "Borderline Low"
-    
-    assert result == expected 
-    
-def test_analyze_HDL_Low():
-    from bloodtests import analyze_HDL
-        
-    result = analyze_HDL(20)
-    expected = "Low"
-    
-    assert result == expected    
 
-    
-    
-    
+@pytest.mark.parametrize("input,expected_output", [
+    (65, "Normal"),
+    (50, "Borderline Low"),
+    (20, "Low"),
+])
+def test_analyze_HDL_Normal(input, expected_output):
+    from bloodtests import analyze_HDL
+    result = analyze_HDL(input)
+
+    assert result == expected_output
