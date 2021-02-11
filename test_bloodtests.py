96978e49636c9bb4e62d7b117f65e6cd194ce15b
Emily Barre <emily.barre@duke.edu>
2021-02-11 18:22:14 -0500

    Added a test for analyzing the LDL level analysis function

--- test_bloodtests.py
+++ test_bloodtests.py
@@ -11,3 +11,15 @@ def test_analyze_HDL_Normal(input, expected_output):
     result = analyze_HDL(input)
 
     assert result == expected_output
+
+@pytest.mark.parametrize("input,expected_output", [
+    (120, "Normal"),
+    (135, "Borderline High"),
+    (165, "High"),
+    (195, "Very High"),
+])
+def test_analyze_LDL_Normal(input, expected_output):
+    from bloodtests import analyze_LDL
+    result = analyze_LDL(input)
+
+    assert result == expected_output
